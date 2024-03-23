962fdd17e16a0eb72e944128bf16536ea033dd82
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-23 17:26:04 +0300

    Задание 3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,30 +1,39 @@
 // Copyright 2021 NNTU-CS
+
 int cbinsearch(int *arr, int size, int value) {
     int count = 0;
+
+
     int low = 0;
     int high = size - 1;
+
+    // Бинарный поиск
     while (low <= high) {
         int mid = low + (high - low) / 2;
+
+
         if (arr[mid] == value) {
             count++;
+
             int left = mid - 1;
             while (left >= low && arr[left] == value) {
                 count++;
                 left--;
             }
+
             int right = mid + 1;
             while (right <= high && arr[right] == value) {
                 count++;
                 right++;
             }
+
             break;
-        }
-        else if (arr[mid] < value) {
+        } else if (arr[mid] < value) {
             low = mid + 1;
-        }
-        else {
+        } else {
             high = mid - 1;
         }
     }
+
     return count;
 }
